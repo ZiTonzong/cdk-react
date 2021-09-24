1e48f05d520f9ee737f731740cff8d2dcf3030f9
cdk <715167986@qq.com>
2021-09-24 16:27:01 +0800

    add 加入级联组件及 unfold 组件

--- src/components/cascader/Cascader.tsx
+++ src/components/cascader/Cascader.tsx
@@ -1,5 +1,10 @@
 import * as PropTypes from 'prop-types'
-
+import * as React from 'react'
+import { classes } from '../../utils'
+import CascaderMenu from './CascaderMenu'
+import Icon from '../icon/Icon'
+import './style'
+import Unfold from '../../transition/Unfold'
 export interface Option {
 	value: string
 	label: string
@@ -78,73 +83,73 @@ class Cascader extends React.Component<CasecaderProps, CascaderState> {
 	}
 
 	// 监听 document, 点击别处关闭
-	public handleClickDocument: EventListener = e => {
-    const target = e.target
-    const { menuVisible } = this.state
-    if (!this.cascaderEl.contains(target as Node) && menuVisible) {
-      this.setState({
-        menuVisible: false
-      })
-    }
-  }
+	public handleClickDocument: EventListener = (e) => {
+		const target = e.target
+		const { menuVisible } = this.state
+		if (!this.cascaderEl.contains(target as Node) && menuVisible) {
+			this.setState({
+				menuVisible: false,
+			})
+		}
+	}
 
-  public saveCascaderEl = (node: HTMLDivElement) => {
-    this.cascaderEl = node
-  }
+	public saveCascaderEl = (node: HTMLDivElement) => {
+		this.cascaderEl = node
+	}
 
-  public handleClickInput = () => {
-    this.setState({
-      menuVisible: !this.state.menuVisible
-    })
-  }
+	public handleClickInput = () => {
+		this.setState({
+			menuVisible: !this.state.menuVisible,
+		})
+	}
 
-  public handleChangeClick = (option: Option, level: number) => {
-    // 最后一级
-    if (!option.children) {
-      this.handleClickLastLevel(option, level)
-    } else {
-      // 中间层级
-      this.handleClickMidLevel(option, level)
-    }
-  }
+	public handleChangeClick = (option: Option, level: number) => {
+		// 最后一级
+		if (!option.children) {
+			this.handleClickLastLevel(option, level)
+		} else {
+			// 中间层级
+			this.handleClickMidLevel(option, level)
+		}
+	}
 
-  // 监听点击最后一层，点击后需要关闭菜单，若 valueArr 改变，需更新输入框显示的值
-  public handleClickLastLevel = (option: Option, level: number) => {
-    const { onChange } = this.props
-    const valueArr = this.state.valueArr || []
-    const selectedOptions = this.state.selectedOptions || []
-    // 相同，未改变，关闭菜单
-    if (valueArr[level] === option.value) {
-      this.setState({
-        menuVisible: false
-      })
-    } else {
-      // 不同，更改后关闭菜单
-      const valueArrCopy = JSON.parse(JSON.stringify(valueArr))
-      const selectedOptionsCopy = JSON.parse(JSON.stringify(selectedOptions))
-      valueArrCopy.splice(level, 1, option.value)
-      selectedOptionsCopy.splice(level, 1, option)
-      this.setState({
-        valueArr: valueArrCopy,
-        selectedOptions: selectedOptionsCopy,
-        inputValueFromLabel: this.getLabelString(selectedOptionsCopy), // 关闭菜单时更新输入框显示的值
-        menuVisible: false
-      })
-      if (onChange) {
-        onChange(valueArrCopy, selectedOptionsCopy)
-      }
-    }
-  }
+	// 监听点击最后一层，点击后需要关闭菜单，若 valueArr 改变，需更新输入框显示的值
+	public handleClickLastLevel = (option: Option, level: number) => {
+		const { onChange } = this.props
+		const valueArr = this.state.valueArr || []
+		const selectedOptions = this.state.selectedOptions || []
+		// 相同，未改变，关闭菜单
+		if (valueArr[level] === option.value) {
+			this.setState({
+				menuVisible: false,
+			})
+		} else {
+			// 不同，更改后关闭菜单
+			const valueArrCopy = JSON.parse(JSON.stringify(valueArr))
+			const selectedOptionsCopy = JSON.parse(JSON.stringify(selectedOptions))
+			valueArrCopy.splice(level, 1, option.value)
+			selectedOptionsCopy.splice(level, 1, option)
+			this.setState({
+				valueArr: valueArrCopy,
+				selectedOptions: selectedOptionsCopy,
+				inputValueFromLabel: this.getLabelString(selectedOptionsCopy), // 关闭菜单时更新输入框显示的值
+				menuVisible: false,
+			})
+			if (onChange) {
+				onChange(valueArrCopy, selectedOptionsCopy)
+			}
+		}
+	}
 
-  // 监听点击中间层，若改变，只在 changeOnSelect 为 true 时更新输入框显示的值
-  public handleClickMidLevel = (option: Option, level: number) => {
-    const { changeOnSelect, onSelect } = this.props
-    const valueArr = this.state.valueArr || []
-    const selectedOptions = this.state.selectedOptions || []
-    // 相同，截掉后面的
-    if (valueArr[level] === option.value) {
-      // 选中即改变时，更新输入框显示的值
-      if (changeOnSelect) {
+	// 监听点击中间层，若改变，只在 changeOnSelect 为 true 时更新输入框显示的值
+	public handleClickMidLevel = (option: Option, level: number) => {
+		const { changeOnSelect, onSelect } = this.props
+		const valueArr = this.state.valueArr || []
+		const selectedOptions = this.state.selectedOptions || []
+		// 相同，截掉后面的
+		if (valueArr[level] === option.value) {
+			// 选中即改变时，更新输入框显示的值
+			if (changeOnSelect) {
 				this.setState({
 					valueArr: valueArr.slice(0, level + 1),
 					selectedOptions: selectedOptions.slice(0, level + 1),
@@ -165,7 +170,7 @@ class Cascader extends React.Component<CasecaderProps, CascaderState> {
 					selectedOptions.slice(0, level + 1)
 				)
 			}
-    } else {
+		} else {
 			// 不同，替换并截掉后面的
 			const valueArrCopy = JSON.parse(JSON.stringify(valueArr))
 			const selectedOptionsCopy = JSON.parse(JSON.stringify(selectedOptions))
@@ -194,5 +199,79 @@ class Cascader extends React.Component<CasecaderProps, CascaderState> {
 				)
 			}
 		}
-  }
+	}
+
+	public getLabelString = (selectedOptions: Option[]): string => {
+		const prev: string[] = []
+		return (
+			selectedOptions
+				.reduce((base, current: Option) => {
+					base.push(current.label)
+					return base
+				}, prev)
+				.join(' / ') || ''
+		)
+	}
+
+	public handleOnClear = () => {
+		this.setState({
+			valueArr: [],
+			selectedOptions: [],
+			inputValueFromLabel: '',
+			menuVisible: false,
+		})
+		if (this.props.onChange) {
+			this.props.onChange([], [])
+		}
+	}
+
+	public render() {
+		const cn = componentName
+		const { options, placeholder, itemClassName, itemStyle, className, style } =
+			this.props
+		const { valueArr, menuVisible, menuStyle, inputValueFromLabel } = this.state
+		return (
+			<div
+				className={classes(cn, '', { active: menuVisible })}
+				ref={this.saveCascaderEl}
+			>
+				<span className={classes(cn, 'input-wrapper')}>
+					{placeholder && !inputValueFromLabel && (
+						<span className={classes(cn, 'placeholder')}>{placeholder}</span>
+					)}
+					<input
+						type="text"
+						className={classes(cn, 'input', [className])}
+					></input>
+					<span className={classes(cn, 'icon-wrapper', ['arrow'])}>
+						<Icon name="arrow" size={8}></Icon>
+					</span>
+					{inputValueFromLabel && (
+						<span
+							className={classes(cn, 'icon-wrapper', ['close'])}
+							onClick={this.handleOnClear}
+						>
+							<Icon name="close" size={10}></Icon>
+						</span>
+					)}
+				</span>
+				{options?.length && (
+					<Unfold visible={menuVisible} vertical={true}>
+						<div className={classes(cn, 'menu-container')} style={menuStyle}>
+							<CascaderMenu
+								options={options}
+								level={0}
+								handleChangeValue={this.handleChangeValue}
+								valueArr={valueArr}
+								itemClassName={itemClassName}
+								itemStyle={itemStyle}
+							></CascaderMenu>
+						</div>
+					</Unfold>
+				)}
+			</div>
+		)
+	}
 }
+
+export default Cascader
