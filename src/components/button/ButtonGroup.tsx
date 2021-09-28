47961346a8d1eb00a02fa7901e10e4f44fec7980
cdk <715167986@qq.com>
2021-09-28 16:47:07 +0800

    add 加入 ButtonGroup 组件

---
+++ src/components/button/ButtonGroup.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import * as PropTypes from 'prop-types'
+import classes from '../../utils/classes'
+
+export interface ButtonGroupProps {
+	className?: string
+	style?: React.CSSProperties
+}
+
+const componentName = 'ButtonGroup'
+
+class ButtonGroup extends React.Component<ButtonGroupProps> {
+	public static displayName = componentName
+
+	public static propTypes = {
+		className: PropTypes.string,
+		style: PropTypes.object,
+	}
+
+	public render() {
+		const { className, style, children } = this.props
+		return (
+			<div className={classes(componentName, '', [className])} style={style}>
+				{children}
+			</div>
+		)
+	}
+}
+
+export default ButtonGroup
